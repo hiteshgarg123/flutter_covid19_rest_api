0ab860c441be2459282e8320001889bbcd92d9cd
Hitesh Garg <hiteshgargvk_cse18@its.edu.in>
2020-09-12 21:03:33 +0530

    Get endpoints data

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_covid19_rest_api/app/services/api.dart';
 
@@ -23,4 +24,45 @@ class APIService {
         'Request ${api.tokenUri()} failed\nResponse : ${response.statusCode} ${response.reasonPhrase}');
     throw response;
   }
+
+  Future<int> getEndPointData({
+    @required String accessToken,
+    @required EndPoint endPoint,
+  }) async {
+    final uri = api.endPointUri(endPoint);
+    final response = await http.get(
+      uri.toString(),
+      headers: {'Authorization': 'Bearer $accessToken'},
+    );
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      if (data.isNotEmpty) {
+        final Map<String, dynamic> endPointData = data[0];
+        final String responseJsonKey = _responseJsonKeys[endPoint];
+        final int result = endPointData[responseJsonKey];
+        if (result != null) {
+          return result;
+        }
+      }
+    }
+    print(
+        'Request $uri failed\nResponse: ${response.statusCode} ${response.reasonPhrase}');
+    throw response;
+  }
+
+  static Map<EndPoint, String> _responseJsonKeys = {
+    EndPoint.cases: 'cases',
+    EndPoint.casesSuspected: 'data',
+    EndPoint.casesConfirmed: 'data',
+    EndPoint.recovered: 'data',
+    EndPoint.deaths: 'data',
+  };
+}
+
+class Celebrate {
+  const Celebrate({@required this.senior});
+  final String senior;
+  void best(String best) => {
+        if (senior == best) {print('Thanks and same to you sir')}
+      };
 }
