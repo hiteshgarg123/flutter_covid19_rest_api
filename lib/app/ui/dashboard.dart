d1e27911b79ab44dafec767546d6a1041035938d
Hitesh Garg <hiteshgargvk_cse18@its.edu.in>
2020-09-18 18:28:40 +0530

    UI update

--- lib/app/ui/dashboard.dart
+++ lib/app/ui/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_covid19_rest_api/app/repositories/data_repository.dart';
+import 'package:flutter_covid19_rest_api/app/repositories/endpoints_data.dart';
 import 'package:flutter_covid19_rest_api/app/services/api.dart';
 import 'package:flutter_covid19_rest_api/app/ui/endpoint_card.dart';
 import 'package:provider/provider.dart';
@@ -10,18 +11,18 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
-  int _cases;
+  EndPointsData _endPointsData;
 
   @override
   void initState() {
     super.initState();
     _updateData();
   }
-  
+
   Future<void> _updateData() async {
     final dataRepository = Provider.of<DataRepository>(context, listen: false);
-    final cases = await dataRepository.getEndPointData(EndPoint.cases);
-    setState(() => _cases = cases);
+    final endPointsData = await dataRepository.getAllEndPointData();
+    setState(() => _endPointsData = endPointsData);
   }
 
   @override
@@ -33,13 +34,19 @@ class _DashBoardState extends State<DashBoard> {
         ),
         centerTitle: true,
       ),
-      body: ListView(
-        children: <Widget>[
-          EndPointCard(
-            endPoint: EndPoint.cases,
-            value: _cases,
-          ),
-        ],
+      body: RefreshIndicator(
+        onRefresh: _updateData,
+        child: ListView(
+          children: <Widget>[
+            for (var endPoint in EndPoint.values)
+              EndPointCard(
+                endPoint: endPoint,
+                value: _endPointsData != null
+                    ? _endPointsData.values[endPoint]
+                    : null,
+              ),
+          ],
+        ),
       ),
     );
   }
--- lib/app/ui/endpoint_card.dart
+++ lib/app/ui/endpoint_card.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_covid19_rest_api/app/services/api.dart';
 
+class EndPointCardData {
+  EndPointCardData({this.title, this.assetName, this.color});
+  final String title;
+  final String assetName;
+  final Color color;
+}
+
 class EndPointCard extends StatelessWidget {
   const EndPointCard({
     Key key,
@@ -11,8 +18,37 @@ class EndPointCard extends StatelessWidget {
   final EndPoint endPoint;
   final int value;
 
+  static Map<EndPoint, EndPointCardData> _cardsData = {
+    EndPoint.cases: EndPointCardData(
+      title: 'Cases',
+      assetName: 'assets/count.png',
+      color: Color(0xFFFFF492),
+    ),
+    EndPoint.casesSuspected: EndPointCardData(
+      title: 'Suspected cases',
+      assetName: 'assets/suspect.png',
+      color: Color(0xFFEEDA28),
+    ),
+    EndPoint.casesConfirmed: EndPointCardData(
+      title: 'Confirmed cases',
+      assetName: 'assets/fever.png',
+      color: Color(0xFFE99600),
+    ),
+    EndPoint.deaths: EndPointCardData(
+      title: 'Deaths',
+      assetName: 'assets/death.png',
+      color: Color(0xFFE40000),
+    ),
+    EndPoint.recovered: EndPointCardData(
+      title: 'Recovered',
+      assetName: 'assets/patient.png',
+      color: Color(0xFF70A901),
+    ),
+  };
+
   @override
   Widget build(BuildContext context) {
+    final cardData = _cardsData[endPoint];
     return Padding(
       padding: const EdgeInsets.symmetric(
         horizontal: 8.0,
@@ -24,16 +60,22 @@ class EndPointCard extends StatelessWidget {
             horizontal: 16.0,
             vertical: 8.0,
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
               Text(
-                'Cases',
+                cardData.title,
                 style: Theme.of(context).textTheme.headline5,
               ),
-              Text(
-                value != null ? value.toString() : '',
-                style: Theme.of(context).textTheme.headline4,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Image.asset(cardData.assetName),
+                  Text(
+                    value != null ? value.toString() : '',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ],
               ),
             ],
           ),
