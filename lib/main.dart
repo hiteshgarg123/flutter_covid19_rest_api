0fb1e59700e7bcb923dc954133fc43742b8d2515
Hitesh Garg <hiteshgargvk_cse18@its.edu.in>
2020-09-20 19:20:52 +0530

    Merged data caching service

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_covid19_rest_api/app/repositories/data_repository.dart';
 import 'package:flutter_covid19_rest_api/app/services/api.dart';
 import 'package:flutter_covid19_rest_api/app/services/api_service.dart';
+import 'package:flutter_covid19_rest_api/app/services/data_cache_service.dart';
 import 'package:flutter_covid19_rest_api/app/ui/dashboard.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final sharedPreferences = await SharedPreferences.getInstance();
+  runApp(
+    MyApp(sharedPreferences: sharedPreferences),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key, @required this.sharedPreferences}) : super(key: key);
+  final SharedPreferences sharedPreferences;
+
   @override
   Widget build(BuildContext context) {
     return Provider<DataRepository>(
       create: (_) => DataRepository(
+        dataCacheService: DataCacheService(
+          sharedPreferences: sharedPreferences,
+        ),
         apiService: APIService(
           API.sandbox(),
         ),
